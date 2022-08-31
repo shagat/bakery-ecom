9aa2fafa5bb5577df1187de40ddcb175f01f1a52
Dave <88671265+shagat@users.noreply.github.com>
2022-08-31 21:23:49 +0530

    skeleton for the layout

--- bakery/src/app/app.module.ts
+++ bakery/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    SideNavComponent
   ],
   imports: [
     BrowserModule,
