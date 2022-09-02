36e18f99d740fb96ac23a7cc526912710038eb77
Dave <88671265+shagat@users.noreply.github.com>
2022-09-02 21:08:58 +0530

    added some data with some styling and also added side nav toggle button with some positioning and styling

--- bakery/src/app/content/content.component.ts
+++ bakery/src/app/content/content.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
-  styleUrls: ['./content.component.scss']
+  styleUrls: ['./content.component.scss'],
 })
 export class ContentComponent implements OnInit {
+  link =
+    'https://images.unsplash.com/photo-1602351447937-745cb720612f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=986&q=80';
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
