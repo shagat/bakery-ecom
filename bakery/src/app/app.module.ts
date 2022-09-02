4131a74551206c2d5fc1fc88150f32cdb3a7c095
Dave <88671265+shagat@users.noreply.github.com>
2022-09-02 14:17:40 +0530

    adding router and httpclient module

--- bakery/src/app/app.module.ts
+++ bakery/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,8 +9,13 @@ import { SideNavComponent } from './side-nav/side-nav.component';
 import { ContentComponent } from './content/content.component';
 
 @NgModule({
-  declarations: [AppComponent, MainComponent, SideNavComponent, ContentComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    MainComponent,
+    SideNavComponent,
+    ContentComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- bakery/src/app/main/main.component.ts
+++ bakery/src/app/main/main.component.ts
@@ -1,17 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss'],
 })
-export class MainComponent {
+export class MainComponent implements OnInit {
   sideNavState = true;
 
-  constructor() {}
+  constructor(private http: HttpClient, private route: ActivatedRoute, private router: Router) {}
+
+  ngOnInit(): void {
+    console.log(this.route.params)
+  }
 
   toggleSideNav() {
     this.sideNavState = !this.sideNavState;
-    console.log(this.sideNavState);
   }
 }
