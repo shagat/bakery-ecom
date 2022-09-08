60c9fb7983fee68bd021c108a7be3bd0964bbded
Dave <88671265+shagat@users.noreply.github.com>
2022-09-08 22:51:38 +0530

    added routing for main page and the item details

--- bakery/src/app/app-routing.module.ts
+++ bakery/src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ItemDetailsComponent } from './items/item-details/item-details.component';
+import { ItemsComponent } from '../app/items/items.component';
+import { ItemListComponent } from './items/item-list/item-list.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: ItemsComponent,
+    children: [
+      { path: '', component: ItemListComponent },
+      { path: ':id', component: ItemDetailsComponent },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- bakery/src/app/app.module.ts
+++ bakery/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatRippleModule } from '@angular/material/core';
 import { FooterComponent } from './footer/footer.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ItemDetailsComponent } from './items/item-details/item-details.component';
 
 @NgModule({
   declarations: [
@@ -25,6 +26,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     ItemComponent,
     DropdownDirective,
     FooterComponent,
+    ItemDetailsComponent,
   ],
   imports: [
     BrowserModule,
